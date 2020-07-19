d7c7ab5d5c2e4aa1377f656a5606998e0578ae31
Doha-Bouhaj <68383105+Doha-Bouhaj@users.noreply.github.com>
2020-07-19 20:35:36 +0100

    Update process.php

--- Feu Tricolore Intelligent/projet/process.php
+++ Feu Tricolore Intelligent/projet/process.php
@@ -9,15 +9,22 @@ if(isset($_POST)){
 				$pwd=$_POST['pwd'];
 				$cni=$_POST['cni'];
 				$grade=$_POST['grade'];
-				$sql="INSERT INTO policier (firstname,lastname,email,pwd,cni,grade) VALUES(?,?,?,?,?,?)";
+				$sql = "SELECT cni FROM policier WHERE cni= '".$cni."' ";
 				$stmtinsert=$db->prepare($sql);
-				$result=$stmtinsert->execute([$firstname,$lastname,$email,$pwd,$cni,$grade]);
+				$resultat= $stmtinsert->execute();
+				if($resultat){
+				$sql="UPDATE policier SET firstname='".$firstname."',lastname='".$lastname."',email='".$email."',pwd='".$pwd."',grade='".$grade."' WHERE cni= '".$cni."'";
+				$stmtinsert=$db->prepare($sql);
+				$result=$stmtinsert->execute();
+			
 				if($result){
 					require_once('verification.php');
 				}
 				else{
 					echo'there were error while savig the data<br/>';
 				}
+				
+			}
 			
 				}else{
 					echo "No data";
